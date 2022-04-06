e316b0788f08d2a67e8412a82d512416de7748b9
techAJAY <ajay2681998@gmail.com>
2022-04-06 18:25:46 +0530

    hello commit

---
+++ src/routes/logger.js
@@ -0,0 +1,7 @@
+let endpoint = "http//:google.com"
+
+let log = function(){
+    console.log("I am fine and you.")
+} 
+
+module.exports.endpoint = endpoint
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+router.get('/test', function (req, res) {
+    console.log('the line is print on terminal')
+    console.log('our server is:',logger.endpoint)
+    console.log("complet code of our first logger")
+    res.send('My first ever api on nodejs!')
+    
+});
+
+router.get('/test1', function (req, res) {
+    res.send('My second ever api on nodejs!')
 });
 
 module.exports = router;
