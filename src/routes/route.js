93d7278af8dd0dae9d30ba4acf10b4de7bbbfcc5
techAJAY <ajay2681998@gmail.com>
2022-04-09 14:58:14 +0530

    hello

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,8 +6,11 @@ const information   = require('../uti/helper')
 const trimming      = require('../validator/formatter')
 const highertolower = require('../validator/formatter')
 const lowertohigher = require('../validator/formatter')
-const lodash        = require('lodash')
+const lodash        = require('lodash');
+const newcontroller = require('../controller/controller');
 
+const { route } = require('express/lib/application');
+const res = require('express/lib/response');
 const router = express.Router();
 
 router.get('/hello', function (req, res) {
@@ -19,6 +22,7 @@ router.get('/hello', function (req, res) {
     trimming.trimfunction()
     highertolower.hightolow()
     lowertohigher.lowtohigh()
+    
 
     let  month = ['jan','feb','mar','apr','may','jun','july','aug','sep','oct','nov','dec']
     let  array = lodash.chunk(month,3)
@@ -35,20 +39,45 @@ router.get('/hello', function (req, res) {
     console.log("your unique number are:",lodash.union(a,b,c,d,e))
     
 
-   // let indexing =  ["horror" , "The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]
-   // console.log("that is your key and value:",lodash.fromPairs(indexing))
+     let indexing =  ["horror" , "The Shining"] ["drama","Titanic"] ["thriller","Shutter Island"] ["fantasy","Pans Labyrinth"]
+    console.log("that is your key and value:",lodash.fromPairs(indexing))
     res.send('my first ever api!')
     
 });
 
-router.get('/test-me', function (req, res) {
-    console.log("--------------------------")
+router.get('/movies', function (req, res) {
+
     console.log(req)
-    console.log("this is your value",req.query)
-    console.log("--------------------------")
+    //console.log("this is your value",req.query)
+     const arr = ['rand de basnasti', 'the shining', 'lord of the rings', 'bartman begins'] 
     
-    res.send('My second ever api on nodejs!')
+    res.send(arr)
 });
+
+
+
+router.get('/movies/:indexnumber', function (req, res) {
+
+    const arr = ['3 idiot','kyo ki','veer zara','one'] 
+    const id = req.params.indexnumber
+    if (id <arr.length){
+        res.send(arr[id])
+        }
+    else {
+        res.send("use valid index")
+        }
+           
+    
+});
+
+//route.post('/test-post3',function(req,res){
+    //newcontroller.sumarr()
+   // res.send({data:newcontroller})
+    //route.post('/getpost-me',function(req,res){
+       // let data  =req.body
+       // console.log(data)
+       // res.send("hi how are you")
+
 
 module.exports = router;
 // adding this comment for no reason
