cd16a07a032d8b1714c84d2cd3e23f34ff4b9f80
techAJAY <ajay2681998@gmail.com>
2022-04-23 01:36:14 +0530

    auth

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://gtgaurav:Wp2gKNWXbHDifb5n@cluster0.9p9yl.mongodb.net/Ajay123-DB", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -15,6 +15,9 @@ const userSchema = new mongoose.Schema( {
         enum: ["male", "female", "other"]
     },
     age: Number,
+    isDeleted : { type: Boolean, default: false},
+    message: {type:[String], deafult: []}
+    
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema)
